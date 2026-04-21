17d4c43d5e07f7e06330b54b433b99a96dfcbde7
Aleatório <lucas.scharf@gmail.com>
2026-04-21 11:02:33 -0300

    preparando ambiente para ter duas implementações de encurtadores

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 mod url_shortener;
+use url_shortener::counter::CounterShortener;
+use url_shortener::UrlShortner;
 
 fn main() {
     println!("Hello, world!");
+
+    let mut shortener = CounterShortener::default();
+    shortener.shorten("URL");
+
+    println!("{shortener:?}");
 }
